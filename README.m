118770db6ed2abe4bb9bc57d690a75e0bfcd8c49
ISAAC OGUSUL <115139453+OGUSULISAAC@users.noreply.github.com>
2023-06-14 10:47:59 +0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,24 @@
+The purpose of the task manager app project is to develop a software application that enables users to effectively manage their daily tasks, enhance productivity, and stay organized.
+
+Inefficient task tracking. Without a proper task management system, it becomes difficult to track the progress of tasks.
+
+Task creation and management:
+	Users can create new tasks with titles, descriptions,due dates and priorities.
+	Tasks can be grouped into projects,tags for better organization.
+Task organization and filtering:
+	Users can organize tasks based on categories and projects.
+	Tasks can be sorted by priority, due date, or other custom criteria.
+
+HTML/CSS: For building the user interface and styling the app.
+JavaScript:To add interactivity and dynamic functionality to the app.
+Database: MySql may be used to store and retrieve task-related data.
+Php:
+
+
+
+
+
+
+
+
+
